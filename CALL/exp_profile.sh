b33f18933e538203cc9b2562ff0efcd993184c9d
StudentServitor <kyle.almryde@gmail.com>
2013-02-03 20:39:46 -0700

    starting construction on a new 'call' system in bash. Got some neat ideas for how to construct subject IDs using brace expansions (They are almost List comprehensions...almost)

--- CALL/exp_profile.sh
+++ CALL/exp_profile.sh
@@ -75,7 +75,7 @@ fi
 # regular processes easier to use and access
 #===============================================================================
 # This function reads an outlier file looking for the lowest integer then prints the NUMBER line
-# - 5 to account for time shifting and AFNI's penchant for starting timeseries with 0 (which is 
+# - 5 to account for time shifting and AFNI's penchant for starting timeseries with 0 (which is
 # annoying), then exits, which causes awk to print only 1 value. This allows us to acquire a base
 # value to register our data to.
 
@@ -91,55 +91,55 @@ function base_reg ()
 
 
 #===============================================================================
-# This function uses the fsl toolbox and "bc" to calculate the Jaccard Index and distance 
+# This function uses the fsl toolbox and "bc" to calculate the Jaccard Index and distance
 # statistics for two binary masks. The Jaccard Index measures the similarity between two binary
-# masks, and the Jaccard Distance calculates the dissimilarity between those two images. 
+# masks, and the Jaccard Distance calculates the dissimilarity between those two images.
 # Note: The scale=6 variable sets the number of decimal places to use.
 # See the link below for more information about Jaccard Index
 # http://en.wikipedia.org/wiki/Jaccard_index
 # Based on a dice_kappa calculation script written by Matt Petoe 11/2/2011
-# Original script ritten by Dianne Patterson 11/9/2011 
+# Original script ritten by Dianne Patterson 11/9/2011
 # Modified by Kyle Almryde 1/18/2012
 
-function jaccard_index () 
+function jaccard_index ()
 {
 	roi1=$1;	roi2=$2
-	
+
 	total_voxels1=`fslstats ${roi1} -V | awk '{printf $1 "\n"}'`
-	echo "total voxels ${roi1} is ${total_voxels1}" 
-	
+	echo "total voxels ${roi1} is ${total_voxels1}"
+
 	total_voxels2=`fslstats ${roi2} -V | awk '{printf $1 "\n"}'`
-	echo "total voxels ${roi2} is ${total_voxels2}" 
-	
+	echo "total voxels ${roi2} is ${total_voxels2}"
+
 	intersect_voxels=`fslstats ${roi1} -k ${roi2} -V | awk '{printf $1 "\n"}'`
 	echo "intersect voxels are ${intersect_voxels}"
-	
+
 	fslmaths ${roi1} -add ${roi2} -bin union
 	union_voxels=`fslstats union -V | awk '{printf $1 "\n"}'`
-	echo "union voxels are ${union_voxels}" 
-	
+	echo "union voxels are ${union_voxels}"
+
 	jaccard_index=`echo "scale=6; ${intersect_voxels}/${union_voxels}" | bc`
 	echo "Jaccard index is ${jaccard_index}"
-	
+
 	jaccard_distance=`echo "scale=6; 1-(${intersect_voxels}/${union_voxels})" | bc`
 	echo "Jaccard distance is ${jaccard_distance}"
-	
+
 	touch jaccard.txt
 	echo "${roi1} ${roi2} ${jaccard_index} ${jaccard_distance}" >>${subrun}.jaccard.txt
 }
 #===============================================================================
-# This string prints the contents of the file (cat), removes columns 10-73 (colrm 10 73: This makes 
+# This string prints the contents of the file (cat), removes columns 10-73 (colrm 10 73: This makes
 # it so only the Volume, Mean, Sem, Max Int, X, Y, and Z are printed) then removes any rows that
 # begin with "#" from the output (sed '/^#/d') ((Note:^ represents the start of the line, the 'd'
 # means delete)) the "." Following that, it replaces any combination of spaces within the text with
-# a single space (tr -s '[:space:]'), which it is itself replaced with a tab (awk -v OFS='\t' 
+# a single space (tr -s '[:space:]'), which it is itself replaced with a tab (awk -v OFS='\t'
 # '$1=$1') before limiting only the first 2 rows of the text (head -2)
 
 function whereami_strip ()
 {
 	cat $1.txt | colrm 10 73 | sed '/^#/d' | tr -s '[:space:]' | awk -v OFS='\t' '$1=$1' \
 		| head -2 > $1.strip.txt
-	
+
 	whereami -tab -atlas TT_Daemon -coord_file $1.strip.txt'[4,5,6]' | sed -n '/TT_Daemon/p' | \
 		awk -v OFS='\t' '$1=$1 {print $2,$4,$5,$6}'| head -4 > $1.whereami.txt
 }
---
+++ call.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#================================================================================
+#    Program Name: call.sh
+#          Author: Kyle Reese Almryde
+#            Date: 02/03/2013 @ 19:41:53 PM
+#
+#     Description: The purpose of this script is to drive various processing and
+#                  analysis steps for fMRI data under study specific conditions.
+#                  It acts in some ways to that of an operator hub which
+#                  coordinates the interactions of variables coming into scope
+#                  with different functions and programs.
+#
+#    Deficiencies: Presently this project is incomplete. Code functionality is
+#                  presently unstable at the moment. Primary construction is
+#                  still incomplete.
+#
+#================================================================================
+#                              FUNCTION DEFINITIONS
+#================================================================================
+
+context=${1}    # This is the study profile (can also be test). It is called
+                # context because it represents the context of with which the
+                # pipelines and function will execute. Or something...
+
+operation=${2}  # Can be either a <Pipeline>, or it can be a <function> both
+                # <<case-sensitive>> Both must exist, whether as a default
+                # Pipleine/Function, or a context specific Pipeline/Function
+
+CONTEXT=/Volumes/Data/${context}
+PROFILE=/usr/local/Utilities/PROFILE
+PROG=/usr/local/Utilities/PROG
+BLOCK=/usr/local/Utilities/BLK
+
+#================================================================================
+#                                 START OF MAIN
+#================================================================================
+
+# Source the context dependent variables and pathnames. A study specific
+source ${PROFILE}/${context}.profile.sh
+
+
+# First check to see if there is a context specific operation, do this for a specific program
+if [[ -e ${PROG}/${context}.${operation}.sh ]]; then
+    . ${PROG}/${context}.${operation}.sh 2>&1 | tee -a ${CONTEXT}/log.${context}.${operation}.txt
+
+# Next check to see if there is a context specific operation, do this for a specific Pipeline
+elif [[ -e ${BLOCK}/${context}.${operation}.sh ]]; then
+    . ${BLOCK}/${context}.${operation}.sh 2>&1 | tee -a ${CONTEXT}/log.${context}.${operation}.txt
+
+# If both of those options fail, check for a default program by that identifier
+elif [[ -e ${PROG}/${operation}.sh ]]; then
+    . ${PROG}/${operation}.sh 2>&1 | tee -a ${CONTEXT}/log.default.${operation}.txt
+
+# Then check for a default Pipeline under that name.
+elif [[ -e ${BLOCK}/${operation}.sh ]]; then
+    . ${BLOCK}/${operation}.sh 2>&1 | tee -a ${CONTEXT}/log.default.${operation}.txt
+
+# Otherwise, display this error message and exit the program.
+else
+    errorMessage()
+fi
+
+#================================================================================
+#                                  END OF MAIN
+#================================================================================
+
+echo "Our Motto"
+echo "We've suffered so you wont have to!"
+echo "Because we put the RE in RESEARCH"
+echo "afterall"
+echo "Its not Rocket science, its BRAIN science!"
+echo ""
+exit 0
