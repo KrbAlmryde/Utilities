ba9867ff497b4296bd470c25ba522bcdfcefe032
Kyle Reese Almryde <kyle.almryde@gmail.com>
2013-04-30 00:12:00 -0700

    Routine commit and backup

--- Iceword/ice.grp.sh
+++ Iceword/ice.grp.sh
@@ -19,7 +19,7 @@
 #================================================================================
 
 
-function group_tTest() {
+function group_tTest () {
     #------------------------------------------------------------------------
     #
     #  Purpose: Perform a one-sample ttest using 3dttest++
@@ -32,8 +32,8 @@ function group_tTest() {
     #------------------------------------------------------------------------
     for delay in {0..7}; do
 
-        mkdir -p ${RESULT}/Ttest/${delay}
-        cd ${RESULT}/Ttest/${delay}
+        mkdir -p ${TTEST}/${delay}
+        cd ${TTEST}/${delay}
 
         listen_array=(`ls ${BASE}/sub0*/${RUN}/*_${delay}sec_ListenStats.nii.gz`)
         response_array=(`ls ${BASE}/sub0*/${RUN}/*_${delay}sec_ResponseStats.nii.gz`)
@@ -89,6 +89,39 @@ function group_tTest() {
                   ${control_array[11]}'[0]' \
                   ${control_array[12]}'[0]' \
                   ${control_array[13]}'[0]'
+
+        3dttest++ \
+            -paired \
+            -prefix tTest_Listen-Response_${delay}sec.nii.gz \
+            -setA ${listen_array[0]}'[0]' \
+                  ${listen_array[1]}'[0]' \
+                  ${listen_array[2]}'[0]' \
+                  ${listen_array[3]}'[0]' \
+                  ${listen_array[4]}'[0]' \
+                  ${listen_array[5]}'[0]' \
+                  ${listen_array[6]}'[0]' \
+                  ${listen_array[7]}'[0]' \
+                  ${listen_array[8]}'[0]' \
+                  ${listen_array[9]}'[0]' \
+                  ${listen_array[10]}'[0]' \
+                  ${listen_array[11]}'[0]' \
+                  ${listen_array[12]}'[0]' \
+                  ${listen_array[13]}'[0]' \
+            -setB ${response_array[0]}'[0]' \
+                  ${response_array[1]}'[0]' \
+                  ${response_array[2]}'[0]' \
+                  ${response_array[3]}'[0]' \
+                  ${response_array[4]}'[0]' \
+                  ${response_array[5]}'[0]' \
+                  ${response_array[6]}'[0]' \
+                  ${response_array[7]}'[0]' \
+                  ${response_array[8]}'[0]' \
+                  ${response_array[9]}'[0]' \
+                  ${response_array[10]}'[0]' \
+                  ${response_array[11]}'[0]' \
+                  ${response_array[12]}'[0]' \
+                  ${response_array[13]}'[0]'
+
     done
 } # End of group_tTest
 
@@ -101,8 +134,8 @@ function group_ANOVA () {
     #=========================================
     for delay in {0..7}; do
 
-        mkdir -p ${RESULT}/${delay}
-        cd ${RESULT}/${delay}
+        mkdir -p ${ANOVA}/${delay}
+        cd ${ANOVA}/${delay}
 
         listen_array=(`ls ${BASE}/sub0*/${RUN}/*_${delay}sec_ListenStats.nii.gz`)
         response_array=(`ls ${BASE}/sub0*/${RUN}/*_${delay}sec_ResponseStats.nii.gz`)
@@ -160,69 +193,6 @@ function group_ANOVA () {
             -acontr 0 1 -1 contrast_${run}_${delay}sec_Rs-Ct.nii.gz
     done
 
-
-
-
-    for delay in {0..7}; do
-
-        mkdir -p ${RESULT}/${delay}
-        cd ${RESULT}/${delay}
-
-        listen_array=(`ls ${BASE}/sub0*/${RUN}/*_${delay}sec_ListenStats.nii.gz`)
-        response_array=(`ls ${BASE}/sub0*/${RUN}/*_${delay}sec_ResponseStats.nii.gz`)
-        control_array=(`ls ${BASE}/sub0*/${RUN}/*_${delay}sec_ControlStats.nii.gz`)
-
-        3dANOVA2 \
-            -type 3 -alevels 3 -blevels 14 \
-            -dset 1 1 ${listen_array[0]}'[0]' \
-            -dset 2 1 ${response_array[0]}'[0]' \
-            -dset 3 1 ${control_array[0]}'[0]' \
-            -dset 1 2 ${listen_array[1]}'[0]' \
-            -dset 2 2 ${response_array[1]}'[0]' \
-            -dset 3 2 ${control_array[1]}'[0]' \
-            -dset 1 3 ${listen_array[2]}'[0]' \
-            -dset 2 3 ${response_array[2]}'[0]' \
-            -dset 3 3 ${control_array[2]}'[0]' \
-            -dset 1 4 ${listen_array[3]}'[0]' \
-            -dset 2 4 ${response_array[3]}'[0]' \
-            -dset 3 4 ${control_array[3]}'[0]' \
-            -dset 1 5 ${listen_array[4]}'[0]' \
-            -dset 2 5 ${response_array[4]}'[0]' \
-            -dset 3 5 ${control_array[4]}'[0]' \
-            -dset 1 6 ${listen_array[5]}'[0]' \
-            -dset 2 6 ${response_array[5]}'[0]' \
-            -dset 3 6 ${control_array[5]}'[0]' \
-            -dset 1 7 ${listen_array[6]}'[0]' \
-            -dset 2 7 ${response_array[6]}'[0]' \
-            -dset 3 7 ${control_array[6]}'[0]' \
-            -dset 1 8 ${listen_array[7]}'[0]' \
-            -dset 2 8 ${response_array[7]}'[0]' \
-            -dset 3 8 ${control_array[7]}'[0]' \
-            -dset 1 9 ${listen_array[8]}'[0]' \
-            -dset 2 9 ${response_array[8]}'[0]' \
-            -dset 3 9 ${control_array[8]}'[0]' \
-            -dset 1 10 ${listen_array[9]}'[0]' \
-            -dset 2 10 ${response_array[9]}'[0]' \
-            -dset 3 10 ${control_array[9]}'[0]' \
-            -dset 1 11 ${listen_array[10]}'[0]' \
-            -dset 2 11 ${response_array[10]}'[0]' \
-            -dset 3 11 ${control_array[10]}'[0]' \
-            -dset 1 12 ${listen_array[11]}'[0]' \
-            -dset 2 12 ${response_array[11]}'[0]' \
-            -dset 3 12 ${control_array[11]}'[0]' \
-            -dset 1 13 ${listen_array[12]}'[0]' \
-            -dset 2 13 ${response_array[12]}'[0]' \
-            -dset 3 13 ${control_array[12]}'[0]' \
-            -dset 1 14 ${listen_array[13]}'[0]' \
-            -dset 2 14 ${response_array[13]}'[0]' \
-            -dset 3 14 ${control_array[13]}'[0]' \
-            -amean 1 mean_${run}_${delay}sec_Listen.nii.gz \
-            -amean 2 mean_${run}_${delay}sec_Response.nii.gz \
-            -amean 3 mean_${run}_${delay}sec_Control.nii.gz \
-            -acontr 1 0 -1 contrast_${run}_${delay}sec_Ln-Ct.nii.gz \
-            -acontr -1 1 0 contrast_${run}_${delay}sec_Rs-Ln.nii.gz \
-            -acontr 0 1 -1 contrast_${run}_${delay}sec_Rs-Ct.nii.gz
-    done
 }
 
 
@@ -244,14 +214,15 @@ function Main () {
         # Define pointers for GLM results #
         #---------------------------------#
         GLM=/Volumes/Data/Iceword/GLM/
-        BASE=/Volumes/Data/Iceword/ANOVA
-        RESULT=/Volumes/Data/Iceword/ANOVA/RESULTS/${RUN}
+        BASE=/Volumes/Data/Iceword/ANALYSIS
+        ANOVA=/Volumes/Data/Iceword/ANALYSIS/ANOVA/${RUN}
+        TTEST=/Volumes/Data/Iceword/ANALYSIS/TTEST/${RUN}
 
         #--------------------#
         # Initiate functions #
         #--------------------#
-        # group_ANOVA 2>&1 | tee -a ${RESULT}/log.txt
-        group_tTest
+        # group_ANOVA 2>&1 | tee -a ${ANOVA}/log_anova.txt
+        group_tTest 2>&1 | tee -a ${ANOVA}/log_ttest.txt
 
     done
 
