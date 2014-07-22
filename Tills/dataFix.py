1f4b36c949f87778ffa4088484b23ee62556a3fa
krbalmryde <kyle.almryde@gmail.com>
2014-07-22 09:54:57 -0700

    Realized I forgot to commit a significantly updated version of this program\! I will never learn....

--- Tills/dataFix.py
+++ Tills/dataFix.py
@@ -12,41 +12,126 @@ Updated: 06/06/2014
               tests they will not have scores computed for them.
 
               Program will check that datasets match in both length and via
-              subject IDs. In the event that an error is encountered, the 
-              program will do 1 of the following: 
+              subject IDs. In the event that an error is encountered, the
+              program will do 1 of the following:
                 1) If the Dataset does not match the person_measure in terms
                    of the number of entries, an error will be reported, and
                    no corrections will be made to that dataset.
-                2) If there is a mismatch in terms of subject IDs in the 
+                2) If there is a mismatch in terms of subject IDs in the
                    dataset against the person_measure, the program will log
                    an error, but WILL report corrections made up to the point
                    of mismatch in the file.
-              In all cases, the program will indicate which Subtest it 
-              encountered the error and in the case of subject ID mismatch, 
-              will also identify the entry number. 
+              In all cases, the program will indicate which Subtest it
+              encountered the error and in the case of subject ID mismatch,
+              will also identify the entry number.
 
           NB: If you encounter a strange error, bug, or problem that you cant
               (or wont) resolve, feel free to contact me via email at:
-               
-               kyle.almryde@gmail.com 
 
-       Usage: Two options, you may navigate to the parent directory of the 
-              files in question, that is, the directory which contains the 
-              data, measure, and final folders and type the following:
-
-                python dataFix.py
-
-              Alternatively, you can provide the path to the directory which
-              contains those folders ala:
-
-                python dataFix.py /Path/To/Parent/Folder
+               kyle.almryde@gmail.com
 
 ==============================================================================
 """
 
-import os, sys
+import os, sys, re
 from glob import glob
 from random import random
+
+
+def getTestID(filePath, pat):
+    """ One line description
+
+    Params:
+        filePath -- arg
+
+
+    Returns:
+         Description of returns
+    """
+    fpl = filePath.lower()
+    _fn = os.path.split(fpl)[1].split('_')[0]
+    sn = re.split(pat, _fn)[1] # sn => subtest number
+    numMatch = re.search('[0-9]+', sn)
+    alphaMatch = re.search('[a-z]+', sn)
+
+    if numMatch and alphaMatch:
+        num, alpha = numMatch.group(), alphaMatch.group()
+        num = str( int(num) ) # convert the string to an int to strip the leading 0 (if there is one)
+        return num+alpha
+
+    elif numMatch and not alphaMatch:
+        num = numMatch.group()
+        return str( int(num) )
+
+    else:
+        alpha = alphaMatch.group()
+        return alpha
+
+
+def makeSubTestIndex(filePath):
+    """ Create a dictionary with Subtest #s as the keys
+        and a list of the data file as values.
+
+    Params:
+        arg -- arg
+
+
+    Returns:
+         Description of returns
+    """
+    testIndex = dict()
+
+    for _file in glob(os.path.join(filePath,"*.txt")):
+        sn = getTestID(_file,'[tT]est')
+        # print "data id is: ",sn, _file
+        testIndex[sn] = [_file]
+
+    return testIndex
+
+
+def makeFinaldirectory(subId, filePath):
+    """
+
+    Params:
+        arg -- arg
+
+
+    Returns:
+         Description of returns
+    """
+    outFn = os.path.join(filePath,"SubTest{0}_results.txt".format(subId))
+    if os.path.isfile(outFn):
+        spacer="                          "
+        print spacer+"WARNING !! {0} already exists! Removing...".format(outFn)
+        os.remove(outFn) # check that the file doenst already exist
+    return outFn
+
+
+
+def loadSubtTestDataFiles(fileList):
+    """ One line description
+
+    Params:
+     arg -- arg
+
+
+    Returns:
+      Description of returns
+    """
+    dFn, iFn, aFn = fileList  # [d]ata,[i]tem,[a]bility [F]ile[n]ame   '\t+|\r\n+|\r+|\n+'
+
+    # _data = open(dFn, 'U').read().strip()
+    _data = open(dFn, 'U').read().strip().split(os.linesep) #re.split('\t+|\r\n+|\r+|\n+', _data)
+
+    # _data = open(iFn, 'U').read().strip()
+    _measures = open(iFn, 'U').read().strip().split()
+    _ability = [ el.split() for el in open(aFn, 'U').read().strip().split(os.linesep)]
+
+    # print "\ndata loaded", _data, "\n\n"
+    # print "\nmeasure loaded", _measures, "\n\n"
+    # print "\nability loaded", _ability, "\n\n"
+    return _data, _measures, _ability
+
 
 
 def writeResults(outFile, data):
@@ -84,30 +169,6 @@ def writeErrorReport(outFile, data):
     report.close()
 
 
-def getModelColumn(subtest_data):
-    """ Computes the modal number of columns extracted from the supplied list
-
-    Params:
-        subtest_data -- List: Contains list of white-space delimited data
-                        columns which can be split. Columns represent subject
-                        metadata as well as testing metrics.
-    Returns:
-        Int -- An integer relaying the modal column length of the data file. 
-               The value is subtracted by 1 to allow for immediate usage as
-               an index to a List.
-    """
-    from collections import Counter
-
-    columnLengths=[]
-    for line in subtest_data:
-        columnLengths.append(len(line.strip().split()))
-    data = Counter(columnLengths)
-    length = data.most_common(1)[0][0]-1
-    print "Modal column length is {0}".format(length)
-    return length  # get the 1st most common item, access
-                                        # the count, which is in index 0 and 0 
-                                        # respectively of a list->tuple pair
-
 
 #=============================== START OF MAIN ===============================
 
@@ -118,95 +179,113 @@ def main():
     DATA = "data"
     MEASURE = "measure"
     FINAL = "final"
+    SubTest_Error_Index = [] # List to keep track of the number of erroneous files that need addressing
 
     # Mainly for testing purposes
-    if len(sys.argv) > 1: 
+    if len(sys.argv) > 1:
         DATA = os.path.join(sys.argv[1], DATA)
         MEASURE = os.path.join(sys.argv[1], MEASURE)
         FINAL = os.path.join(sys.argv[1], FINAL)
 
+    # checks to see if the 'final' directory exists, creating it if necessary
+    if not os.path.exists(FINAL): os.mkdir(FINAL)
 
+    # create { SubTest: [File names] dictionary }
+    SubTest_Index = makeSubTestIndex(DATA)
 
-    if not os.path.exists(FINAL): os.mkdir(FINAL)  # checks to see if the 'final' directory exists, creating it if necessary
-
-    # Create a dictionary with Subtest #s as the keys and a list of the data
-    # file as values. Uses a Dictionary Comprehension
-    SubTestIndex = {os.path.split(_file)[1].split('_')[0].split('Test')[1]: [_file] for _file in glob(os.path.join(DATA,"*.txt"))}       
-
+    # add measures filenames to SubTest_Index
     for measure in glob(os.path.join(MEASURE,"*.txt")):
-        sn = os.path.split(measure)[1].split('_')[0].split('Sub')[1] # sn => subtest number
-        SubTestIndex[sn].append(measure)  # append the subtest Item and Person
-                                          # measure to the SubTestIndex
+        sn = getTestID(measure, '[sS]ub')
+        # print "measure id is: ",sn, measure
+        SubTest_Index[sn].append(measure)  # append the subtest Item and Person measure to the SubTest_Index
 
-        # SubTestIndex should look something like this:
-        # {'Sub01': ['SubTest01_SC.txt', 'Sub01_item_meausre.txt', 'Sub01_person_meausre.txt']}
+        # SubTest_Index should look something like this:
+        # {'sub1': ['SubTest01_SC.txt', 'Sub01_item_meausre.txt', 'Sub01_person_meausre.txt'],
+        #  'sub3a': ['SubTest3A_SC.txt', 'Sub3A_item_meausre.txt', 'Sub3A_person_meausre.txt']}
+    # print "\n",len(SubTestIndex), "SubTestIndex", SubTestIndex
 
-    SubTestErrorIndex = [] # List to keep track of the number of erroneous files that need addressing
 
+    # Start main loop
+    for subId, file_name_List in SubTest_Index.items():
+        spacer="                          "
+        has_Error=0  # effectively an "ignore me" flag
+        print "\n++++++++++++++++++++++++++++ SubTest:", subId, len(file_name_List)
 
-    # k is the key, ie Sub01, v is a 3 element list containing the filenames associated with each subtest
-    for k,v in SubTestIndex.items():
-        if len(v) < 2: pass
+        if len(file_name_List) < 3:
+            has_Error = 1
+            SubTest_Error_Index.append((file_name_List, "Files(s) Error"))
+
+            print spacer+"ERROR !! Missing,", 3 - len(file_name_List), "SubTest measure files!!", file_name_List
+            pass
         else:
-            outFn = os.path.join(FINAL,"SubTest{0}_results.txt".format(k))
-            if os.path.isfile(outFn):
-                print "{0} already exists! Removing...".format(outFn)
-                os.remove(outFn) # check that the file doenst already exist
-            
-            dFn, iFn, aFn = v  # [d]ata,[i]tem,[a]bility [F]ile[n]ame
-            subtest_data = open(dFn, 'U').read().strip().split('\n')
-            items_measures = open(iFn, 'U').read().strip().split()
-            persons_ability = open(aFn, 'U').read().strip().split('\n')
+            # make the 'final' directory if it doesnt already exist
+            output_Filename = makeFinaldirectory(subId, FINAL)
 
+            # load subtest data, item measure, and person ability scores
+            subtest_data, items_measures, persons_ability = loadSubtTestDataFiles(file_name_List)
+
+            # Check that subtest_data is the same size as the persons_ability
             if len(subtest_data) != len(persons_ability):
-                SubTestErrorIndex.append((dFn, "Size Mismatch Error"))
-                print "!! Number of entries in SubTest data does not match number of entries in person ability data for: {0}!!".format(k)
-                print "\tThere are currently {0} erroneous SubTests that need to be addressed".format(len(SubTestErrorIndex))
-                break
+                has_Error = 1
+                SubTest_Error_Index.append((file_name_List[0], "Size Mismatch Error:\tdata: {0}\tability: {1}".format(len(subtest_data), len(persons_ability))))
+                print spacer+"ERROR !! Size Mismatch Error:\tdata: {0}\tability: {1}".format(len(subtest_data), len(persons_ability))
+                # print "\tThere are currently {0} erroneous SubTests that need to be addressed\n".format(len(SubTest_Error_Index))
+                pass
 
-            index = getModelColumn(subtest_data) # get the modal column length by which to index the data 
             for i, line in enumerate(subtest_data):
-                # print "line is", line
-                seg = line.strip().split()   # Each line represents a single subject, strip off any newline characters, the split on whitespace
-                data = "".join(seg[9:])  #+ some data files have spaces intermixed in the data column, resulting in
-                                             #+ potentially missed values, this line corrects that.subtest_data[i]
-                subtest_data[i] = [seg[0], data]
-                persons_ability[i] = persons_ability[i].split()
 
-                result = "{0}\t".format(subtest_data[i][0])
-                
-                # print result
-                if subtest_data[i][0] != persons_ability[i][0]:  # if the subtest participant id does not match the person id, log the error!
-                    SubTestErrorIndex.append((dFn, "Participant ID Mismatch Error: line {0}".format(i)))
-                    print "!! {0} participant ID does not match {1} participant ID: line {2}!!".format(dFn, aFn, i)
-                    print "\t",repr(subtest_data[i][0]), repr(persons_ability[i][0])
-                    print "\tThere are currently {0} erroneous SubTests that need to be addressed".format(len(SubTestErrorIndex))
-                    break
-
-                if persons_ability[i][1] in ["X","x","?"]:  # If the ability file has an X, skip it. Subj never took the test
-                    # print "Subject didnt take test"
-                    writeResults(outFn, result+data)
+                if has_Error:
+                    pass
                 else:
-                    for j, point in enumerate(data):
-                        if point != '.':    # If the value is not a '.' write that value
-                            result += point
-                        else:
-                            if float(persons_ability[i][1]) < float(items_measures[j]):  # if ability is less than measure, assign 0
-                                result += '0'
-                            elif float(persons_ability[i][1]) >  float(items_measures[j]): # if ability is greater than measure, assign 1
-                                result += '1'
-                            else:  # if ability is equal to measure...
-                                if random() < 0.5:  # assign a 0 if a randomly generated value between 0-1 is less than 0.5
-                                    result += "0"  # Failed
-                                else: # assign a 1 if a randomly generated value between 0-1 is greater than 0.5
-                                    result += "1"  # Passed
+                    seg = line.strip().split()  # Each line represents a single subject, strip off any newline characters, the split on whitespace
+                    data = "".join(seg[9:])  # some data files have spaces intermixed in the data column, resulting in
+                                             # potentially missed values, this line corrects that.subtest_data[i]
+                    subtest_data[i] = [seg[0], data]
+                    # print "subtest_data[{0}]: \t{1}".format(i, subtest_data[i])
+                    # persons_ability[i] = re.split('\t+|\r\n+|\r+|\n+', persons_ability[i])
+                    result = "{0}\t".format(subtest_data[i][0])
 
-                    writeResults(outFn, result)  # write final results to file
-            
-    if SubTestErrorIndex:
-        print "There were errors in the program! Writing error report..."
+                    # print "\tSubTest subID:",repr(subtest_data[i][0]), len(subtest_data[i][0])
+                    # print "\tperson subID:", repr(persons_ability[i][0]), len(persons_ability[i][0])
+
+                    # Check that if there is a mismatch between subtest_data[i][0] and persons_ability[i][0] subIds
+                    if subtest_data[i][0] != persons_ability[i][0]:  # if the subtest participant id does not match the person id, log the error!
+                        has_Error = 1
+                        SubTest_Error_Index.append((dFn, "Participant ID Mismatch Error: line {0}".format(i)))
+                        print spacer+"ERROR !! Participant ID Mismatch Error:{0} participant ID does not match {1} participant ID: line {2}!!".format(dFn, aFn, i)
+                        # print "\t",repr(subtest_data[i][0]), repr(persons_ability[i][0])
+                        # print "\tThere are currently {0} erroneous SubTests that need to be addressed\n".format(len(SubTest_Error_Index))
+                        break
+
+                    # Perform measure corrections and write to file, Or report error and skip subject
+                    if len(items_measures) != len(subtest_data[i][1]):
+                        has_Error = 1
+                        SubTest_Error_Index.append((file_name_List[i], "Item Measure Mismatch:\titem: {0}\tdata: {1}".format(len(items_measures), len(subtest_data[i][1]))))
+                        print spacer+"ERROR !! Item Measure Mismatch:\titem: {0}\tdata: {1}".format(len(items_measures), len(subtest_data[i][1]))
+                        pass
+                    elif persons_ability[i][1] in ["X","x","?"]:  # If the ability file has an X, skip it. Subj never took the test
+                        # print "Subject didnt take test"
+                        writeResults(output_Filename, result+subtest_data[i][1])
+                    else:
+                        for j, point in enumerate(subtest_data[i][1]):
+                            if point != '.':    # If the value is not a '.' write that value
+                                result += point
+                            else:
+                                if float(persons_ability[i][1]) < float(items_measures[j]):  # if ability is less than measure, assign 0
+                                    result += '0'
+                                elif float(persons_ability[i][1]) >  float(items_measures[j]): # if ability is greater than measure, assign 1
+                                    result += '1'
+                                else:  # if ability is equal to measure...
+                                    if random() < 0.5:  # assign a 0 if a randomly generated value between 0-1 is less than 0.5
+                                        result += "0"  # Failed
+                                    else: # assign a 1 if a randomly generated value between 0-1 is greater than 0.5
+                                        result += "1"  # Passed
+                        writeResults(output_Filename, result)  # write final results to file
+        has_Error=0
+    if SubTest_Error_Index:
+        print "\nThere were errors {0} in the program! Writing error report...".format(len(SubTest_Error_Index))
         errorFN = os.path.join(FINAL,"ErrorReport_")
-        writeErrorReport(errorFN, SubTestErrorIndex) # Write 
+        writeErrorReport(errorFN, SubTest_Error_Index) # Write
     else:
         print "There were no errors detected! Be sure to look over your results for accuracy!"
 
